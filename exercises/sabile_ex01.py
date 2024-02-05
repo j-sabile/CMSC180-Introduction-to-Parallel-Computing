80fc636a86bab2db4301c2cd912d730dbdfcab81
j-sabile <jericosabile@gmail.com>
2024-02-05 11:52:34 +0800

    add working correct

--- exercises/sabile_ex01.py
+++ exercises/sabile_ex01.py
@@ -1,10 +1,47 @@
+# WORKING AND CORRECT
 def pearson_cor(x, y, m, n):
     v = []
     for i in range(n):
-        v.append(0)
-        for j in range(m):
-            v[i] = (m * getXjy(x,y,m,j) - getXj(x,m,j) * getY(y, m)) / ((m * getX2j(x,m,j) - getX(x)**2) * (m * getY2(y,m) - getY(y,m)**2)) **0.5
+        v.append((m*sumXY(x,y,m,i)-sumX(x,m,i)*sumY(y,m))/((m*sumX2(x,m,i)-sumX(x,m,i)**2)*((m*sumY2(y,m))-sumY(y,m)**2))**0.5)
     return v
+
+def sumY2(y,m):
+    sum = 0
+    for i in range(m): sum += y[i]**2
+    return sum
+
+def sumX2(x,m,j):
+    sum = 0
+    for i in range(m): sum += x[j][i]**2
+    return sum
+
+def sumXY(x,y,m,j):
+    sum = 0
+    for i in range(m): sum += x[j][i] * y[i] 
+    return sum
+
+def sumX(x,m,j):
+    sum = 0
+    for i in range(m): sum += x[j][i]
+    return sum
+
+def sumY(y,m):
+    sum = 0
+    for i in range(m): sum += y[i]
+    return sum
+
+
+###########################################
+
+
+# WORKING BUT INCORRECT?
+# def pearson_cor(x, y, m, n):
+#     v = []
+#     for i in range(n):
+#         v.append(0)
+#         for j in range(m):
+#             v[i] = (m * getXjy(x,y,m,j) - getXj(x,m,j) * getY(y, m)) / ((m * getX2j(x,m,j) - getX(x)**2) * (m * getY2(y,m) - getY(y,m)**2)) **0.5
+#     return v
 
 def getXjy(x, y, m, j):
     sum = 0
@@ -59,10 +96,10 @@ def getY(y, m):
 
 
 
-matrix = [[9,5,3],[4,2,6],[7,8,9]]
-y = [3,1,4]
-m = 3
-n = 3
+matrix = [[3.63,3.02,3.82,3.42,3.59,2.87,3.03,3.46,3.36,3.3]]
+y = [53.1,49.7,48.4,54.2,54.9,43.7,47.2,45.2,54.4,50.4]
+m = 10
+n = 1
 
 ans = pearson_cor(matrix, y, m, n)
 print(ans)
