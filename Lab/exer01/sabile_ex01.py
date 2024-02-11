0841319a38bbf4e471fb08262f2025b7e8544fcb
j-sabile <jericosabile@gmail.com>
2024-02-12 03:51:32 +0800

    finished exer1

--- Lab/exer01/sabile_ex01.py
+++ Lab/exer01/sabile_ex01.py
@@ -6,7 +6,6 @@
 import time
 import random
 
-# WORKING AND CORRECT
 def pearson_cor(x, y, m, n):
     v = []
     for i in range(n):
@@ -38,56 +37,6 @@ def sumY(y,m):
     for i in range(m): sum += y[i]
     return sum
 
-
-###########################################
-
-# WORKING BUT INCORRECT?
-def pearson_cor2(x, y, m, n):
-    v = []
-    for i in range(n):
-        v.append(0)
-        for j in range(m):
-            v[i] = (m * getXjy(x,y,m,j) - getXj(x,m,j) * getY(y, m)) / ((m * getX2j(x,m,j) - getX(x)**2) * (m * getY2(y,m) - getY(y,m)**2)) **0.5
-    return v
-
-def getXjy(x, y, m, j):
-    sum = 0
-    for i in range(m): sum += x[i][j] * y[i] 
-    return sum
-
-def getXj(x, m, j):
-    sum = 0
-    for i in range(m): sum += x[i][j]
-    return sum
-
-def getY(y, m):
-    sum = 0
-    for i in range(m): sum += y[i]
-    return sum
-
-def getX2j(x, m, j):
-    sum = 0
-    for i in range(m): sum += (x[i][j])**2
-    return sum
-
-def getX(x):
-    sum = 0
-    for i in x: 
-        for j in i: sum += j
-    return sum
-
-def getY2(y, m):
-    sum = 0
-    for i in range(m): sum += y[i] ** 2
-    return sum
-
-def getY(y, m):
-    sum = 0
-    for i in range(m): sum += y[i]
-    return sum
-
-###########################################
-
 def generateRandom(x, y):
     numbers = []
     for i in range(x):
@@ -98,21 +47,17 @@ def generateRandom(x, y):
             numbers.append(new)
     return numbers 
 
-
+# TEST CASE
+# ANSWER: 0.47
 # matrix = [[3.63,3.02,3.82,3.42,3.59,2.87,3.03,3.46,3.36,3.3]]
 # y = [53.1,49.7,48.4,54.2,54.9,43.7,47.2,45.2,54.4,50.4]
 # m = 10
 # n = 1
 
 size = int(input("Size: "))
-
 matrix = generateRandom(size, size)
 y = generateRandom(size, 0)
-m = size
-n = size
-# print(matrix)
-# print(y)
-
+m = n = size
 
 start_time = time.time()
 ans = pearson_cor(matrix, y, m, n)
