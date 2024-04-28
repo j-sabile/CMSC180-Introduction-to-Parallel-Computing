032280383723518fa9e3e29d8ab6c6155f79241d
Jerico Sabile <96747804+j-sabile@users.noreply.github.com>
2024-04-28 18:49:40 +0000

    remove printing of matrix

--- Lab/exer04/exer04.c
+++ Lab/exer04/exer04.c
@@ -193,10 +193,10 @@ float* slaveFunc(int connfd, int* n) {
     int ack = 1;
     write(connfd, &ack, sizeof(int));
     printf("\nPrinting the matrix received\n");
-    for(int i=0; i<m; i++) {
-        for (int j=0; j<(int)*n; j++) printf("%d ", matrix[i][j]);
-        printf("\n");
-    }
+    // for(int i=0; i<m; i++) {
+    //     for (int j=0; j<(int)*n; j++) printf("%d ", matrix[i][j]);
+    //     printf("\n");
+    // }
 
     pearson_cor_basic(matrix, y, v, m, (int)*n);
     for (int i=0; i<m; i++) free(matrix[i]);
